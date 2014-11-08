17bc1ae36187580b35f2d3bff3dccbc533ed0ec3
Chris Pollard <cspollard@gmail.com>
2014-11-08 18:42:32 +0000

    cleaining up Event.hs

--- Data/HepMC/Event.hs
+++ Data/HepMC/Event.hs
@@ -18,13 +18,3 @@ data Event = Event {
 
 parserEvent :: Parser Event
 parserEvent = Event <$> parserEventHeader <*> (fromList <$> many parserVertex)
-
-    {-
-    let insertVert v m = IM.insert (vertexBarcode v) v m
-    let insertParts ps m = foldr (\p m' -> IM.insert (partBarcode p) p m') m ps
-    let insertVertParts (v, ps) (vmap, pmap) = (insertVert v vmap, insertParts ps pmap)
-
-    let (verts, parts) = foldr insertVertParts (IM.empty, IM.empty) vertparts
-
-    return $ Event header verts
-    -}
