1dfb3f47573c2544d959ff24cfb51356ab0d42bf
Chris Pollard <cspollard@gmail.com>
2016-01-14 17:36:48 +0000

    forgot a file.

---
+++ src/Data/HepMC/EventGraph.hs
@@ -0,0 +1,49 @@
+module Data.HepMC.EventGraph where
+
+import Data.HepMC.Parse
+import qualified Data.Set as S
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IM
+import Data.HepMC.Vertex
+import Data.HepMC.Barcoded
+import qualified Data.HEP.PID as PID
+import Data.HEP.PID (HasPID(..))
+import Data.Either (partitionEithers)
+
+
+-- do we need an IntMap of these guys?
+data EventGraph = EventGraph {
+    egVerts :: Vertices,
+    egParts :: Particles,
+    egVertsMap :: IntMap Vertex,
+    egPartsMap :: IntMap Particle
+}
+
+
+-- egFinalParts :: EventGraph -> Particles
+-- egFinalParts = filter (null . partChildVert) . egParts
+
+parents, children, descendants, ancestors :: Particle -> Particles
+
+parents = vertParentParts . partParentVert
+children = vertChildParts . partChildVert
+
+descendants n = S.toList $ descendants' S.empty n
+    where
+        -- ignore nodes that are already in the set---avoid loops.
+        descendants' s n' = if n' `S.member` s
+                            then s
+                            else foldl descendants' (n' `S.insert` s) (children n')
+
+ancestors n = S.toList $ foldl ancestors' S.empty (parents n)
+    where
+        -- ignore nodes that are already in the set---avoid loops.
+        ancestors' s n' = if n' `S.member` s
+                            then s
+                            else foldl ancestors' (n' `S.insert` s) (parents n')
+
+
+-- TODO
+-- this needs to be looked into.
+-- isPrompt :: Particle -> Bool
+-- isPrompt p = null . filter (\p' -> partStatus p' == 2 && (PID.isHadron p' || PID.isTau p')) . ancestors $ p
