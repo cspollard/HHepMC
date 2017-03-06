209bd9b397a0379145c2b0cf5b71ca6dcbe6f9fd
Chris Pollard <cspollard@gmail.com>
2017-03-06 09:11:49 +0000

    compiling in lts-8.0

--- test/hepmc-test.hs
+++ test/hepmc-test.hs
@@ -1,20 +1,16 @@
 module Main where
 
-import Control.Lens hiding (children)
+import           Control.Lens            hiding (children)
 
-import Conduit
-import Data.Conduit.Attoparsec
+import           Conduit
+import           Data.Conduit.Attoparsec
 
-import Data.List (sortOn)
+import           Data.HEP.PID
 
-import Data.HEP.LorentzVector
-import Data.HEP.PID
-
-import Data.HepMC.Parse
-import Data.HepMC.Barcoded
-import Data.HepMC.Event
-import Data.HepMC.EventGraph
-import System.Environment (getArgs)
+import           Data.HepMC.Event
+import           Data.HepMC.EventGraph
+import           Data.HepMC.Parse
+import           System.Environment      (getArgs)
 
 
 main :: IO ()
@@ -27,9 +23,10 @@ main = do
 
             =$= catEithersC
             =$= mapC snd
-            =$= mapC (toListOf (particles.filtered promptLepton))
-            =$= mapC (sortOn (negate . view lvPt))
-            $$  mapM_C (liftIO . mapM_ (views bc print))
+            =$= mapC (toListOf (particles.filtered ewDecay.children.pid))
+            -- =$= mapC (sortOn (negate . view lvPt))
+            $$  mapM_C (liftIO . print)
+            -- $$  mapM_C (liftIO . mapM_ (views bc print))
             -- $$  mapM_C (liftIO . findZll)
 
     where catEithersC = do
@@ -38,15 +35,12 @@ main = do
                 Just (Right y) -> yield y >> catEithersC
                 _              -> return ()
 
+finalWith :: (Particle -> Bool) -> Particle -> Bool
+finalWith f p = f p && (not . any f $ toListOf children p)
 
-promptLepton :: Particle -> Bool
-promptLepton = and . sequenceA [isChargedLepton, final, not . fromHadron]
 
-findZll :: Event -> IO ()
-findZll e = case e ^.. particles . filtered promptLepton of
-                ls@[_, _] -> do
-                        traverseOf_ (traverse . pid) print ls
-                        views lvM print (foldOf (traverse . toXYZT) ls)
-
-                _ -> print "nope"
-
+ewDecay :: Particle -> Bool
+ewDecay =
+  or
+    . traverse finalWith
+      (classOf <$> [bottomHadron, charmHadron, tau, anti tau])
